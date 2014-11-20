fe9e54a2d94b73e8cd3c3a952cb4447e934eca1e
mayoms <mayoms@gmail.com>
2014-11-19 19:55:05 -0800

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -1,118 +1,200 @@
-from random import randint
 from random import randrange
 
-ships = 0
-board = []
-BuiltShips = {}
-board_size = int(input("How big would you like the board to be?"))
-for x in range(board_size):
-    board.append(["O"] * board_size)
-def print_board(board):
-    for row in board:
-        print(" ".join(row))
+def force_int(n):
+    try:
+        return int(n)
+    except ValueError:
+        return force_int(input("Sorry, not a number. Try again:"))
+
+def excluderand(excluded):
+    coordinates = None
+    while coordinates in excluded or coordinates is None:
+        coordinates = randrange(0, my_board.board_size - 1)
+    return coordinates
 
 
-class BattleShip(object):
-    def __init__(self, id):
-        self.id = id
-        self.location = {
-            "x": [],
-            "y": []
-        }
-        self.hits = 0
+class Board(object):
+    def __init__(self):
+        self.board_size = 0
+        self.ship_count = 0
+        self.board = []
+        self.ships = []
+        self.occupied = {"x":[], "y":[]}
+
+    def build_board(self):
+        self.board = []
+        self.board_size = force_int(input("How big would you like the board to be?"))
+        if self.board_size < 5:
+            print("The minimum size is 5x5")
+            self.board_size = 5
+        for column in range(self.board_size):
+            self.board.append(["O"] * self.board_size)
+
+    def print_board(self):
+        for row in self.board:
+            print(" ".join(row))
+
+    def populate(self):
+        self.ships = []
+        self.ship_count = force_int(input("How many ships would you like?"))
+        while self.ship_count > self.board_size // 2:
+            print("That's a few too many for your board size, try between 1 and %s" % (self.board_size // 2))
+            self.ship_count = force_int(input("How many ships would you like?"))
+        else:
+            for each in range(self.ship_count):
+                newship = Ship()
+                newship.orient_ship()
+                if newship.orientation == "vertical":
+                    newship.vlocation()
+                else:
+                    newship.hlocation()
+                self.ships.append(newship)
+                print (self.ships[each].x, self.ships[each].y)
+
+    def update_board(self, x, y, result):
+        if result == "hit":
+            self.board[x][y] = "!"
+        else:
+            self.board[x][y] = "X"
+
+class Ship(object):
+    def __init__(self):
+        self.x = []
+        self.y = []
         self.orientation = ""
-    x = [] # Keep Track of all X Coordinates
-    y = [] # Keep Track of all Y Coordinates
-    sank = 0
-           # All battleships start with these attributes, hit count, whether or not it is sank, location and orientation
-    def ExcludeRand(self,exclude): #this will generate a random number, while excluding coordinates already assigned
-        points = None
-        while points in exclude or points is None:
-            points = randrange(0, len(board)-1)
-        return points
+        self.hits = 0
 
-    # Battleship, Build Thyself!
-    def build(self):
-        if randint(0, 1) == 1:  # Flip a coin to determine orientation
+    def orient_ship(self):
+        if randrange(0,2) == 1:
             self.orientation = "vertical"
         else:
             self.orientation = "horizontal"
 
-      # If there aren't any ships built yet, we can put it anywhere
-        if self.orientation == "horizontal":  #If the coin flipped to horizontal, build it that way
-            self.location["x"].append(int(self.ExcludeRand(self.x)))  #Assign Random X Coordinate
-            self.x.append(self.location["x"][0])
-            print ("X's:", self.x)
-            print (self.location)
-            self.location["y"].append(int(self.ExcludeRand(self.y)))  #Assign Random Y Coordinate
-            self.y.append(self.location["y"][0])
-            print (self.location)
-            if self.location["x"] == len(board) - 1:
-                self.location["x"][0].append(len(board) - 2)
-                print (self.location)
+    def hlocation(self):
+        self.x.append(int(excluderand(my_board.occupied["x"])))
+        my_board.occupied["x"].append(self.x[0])
+        self.y.append(int(excluderand(my_board.occupied["y"])))
+        my_board.occupied["y"].append(self.y[0])
+        if self.x[0] == my_board.board_size:
+            self.x.append(my_board.board_size - 1)
+            my_board.occupied["x"].append(self.x[1])
+        else:
+            self.x.append(self.x[0] + 1)
+            my_board.occupied["x"].append(self.x[1])
+
+    def vlocation(self):
+        self.x.append(int(excluderand(my_board.occupied["x"])))
+        my_board.occupied["x"].append(self.x[0])
+        self.y.append(int(excluderand(my_board.occupied["y"])))
+        my_board.occupied["y"].append(self.y[0])
+        if self.y[0] == my_board.board_size:
+            self.y.append(my_board.board_size -1)
+            my_board.occupied["y"].append(self.y[1])
+        else:
+            self.y.append(self.y[0] + 1)
+            my_board.occupied["y"].append(self.y[1])
+
+class Player(object):
+    def __init__(self, name):
+        self.name = name
+        self.wins = 0
+        self.losses = 0
+        self.tries = 0
+
+    def calculate_stats(self):
+        pass
+
+    def print_stats(self):
+        print("Stats for %s" % self.name)
+        print("Games Played: %s" % self.tries)
+        print("Games Won: %s" % self.wins)
+        print("Games Lost: %s" % self.losses)
+        print("Win Percentage: %s" % (self.wins / self.tries))
+    def update_stat_board(self):
+        pass
+
+    def print_leaderboard(self):
+        pass
+
+class Game(object):
+    def __init__(self):
+        self.turns = 0
+        self.sank = 0
+        self.hits = 0
+        self.misses = 0
+
+    def play(self):
+        self.sank = 0
+        self.hits = 0
+        print ("Shall we play a game?")
+        my_board.build_board()
+        my_board.populate()
+        self.turns = 3 * my_board.ship_count
+        current_player.tries += 1
+        while self.sank < my_board.ship_count:
+            if self.turns > 0:
+                print ("%s turns left" % self.turns)
+                my_board.print_board()
+                guess_x = force_int(input("Guess Row: "))
+                guess_y = force_int(input("Guess Column: "))
+                if guess_x > my_board.board_size or guess_y > my_board.board_size:
+                    print ("That's not even on the map!")
+                    self.turns -=1
+                elif my_board.board[guess_x][guess_y] == "X" or my_board.board[guess_x][guess_y] == "!":
+                    print ("You already guessed there!")
+                    self.turns -=1
+                elif self.assault(guess_x,guess_y) is True:
+                    print ("You got a hit!")
+                    my_board.update_board(guess_x,guess_y,"hit")
+                    self.hits +=1
+                else:
+                    print ("Miss!")
+                    my_board.update_board(guess_x,guess_y,"miss")
+                    self.misses +=1
+                    self.turns -=1
             else:
-                self.location["x"].append(self.location["x"][0] + 1)
-                print (self.location)
-            print (self.location)
+                print ("Sorry, out of turns.")
+                current_player.losses += 1
+                self.play_again(input("Would you like to play again?"))
+                break
         else:
-            self.location["x"].append(int(self.ExcludeRand(self.x)))  #Random X
-            self.x.append(self.location["x"][0])
-            print (self.location)
-            self.location["y"].append(int(self.ExcludeRand(self.y)))  #Random Y
-            self.y.append(self.location["y"][0])
-            print ("Y's:",self.y)
-            print (self.location)
-            if self.location["y"][0] == len(board) - 1:  #Y plus or minus 1
-                self.location["y"].append(len(board) - 2)
-                print (self.location)
-            else:
-                self.location["y"].append(self.location["y"][0] + 1)
-                print (self.location)
+            print ("You won!")
+            current_player.wins += 1
+            self.play_again(input("Would you like to play again?"))
 
-def is_int(n):
-    try:
-        return int(n)
-    except ValueError:
-        is_int(input("Sorry, not a number. Try again:"))
+    def assault(self,x,y):
+        for each in range(my_board.ship_count):
+            print (each)
+            print (my_board.ships[each].x, my_board.ships[each].y)
+            if x in my_board.ships[each].x and y in my_board.ships[each].y:
+                my_board.ships[each].hits +=1
+                self.hits +=1
+                if my_board.ships[each].hits == 2:
+                    print("You sank a ship!")
+                    self.sank +=1
+                return True
+        return False
 
-ships = is_int(input("How many ships?"))
-
-for each in range(ships):
-    BuiltShips["ship" + str(each)] = BattleShip(each)
-    BuiltShips["ship" + str(each)].build()
-def Assault(x,y):
-    for each in BuiltShips:
-        if x in BuiltShips[each].location["x"] and y in BuiltShips[each].location["y"]:
-            BuiltShips[each].hits += 1
-            if BuiltShips[each].hits == 2:
-                print ("You sank a ship!")
-                BattleShip.sank += 1
-            return True
+    def play_again(self,answer):
+        if answer.lower() == "y":
+            my_game.play()
+        elif answer.lower() == "n":
+            current_player.print_stats()
         else:
-            print (BuiltShips[each].location)
-    return False
+            print("Sorry, please enter 'y' for yes or 'n' for no")
+            self.play_again(input("Play again?"))
 
-turns = 3 * ships
-while BattleShip.sank < ships:
-    if turns > 0:
-        print ("%s turns left" % turns)
-        print_board(board)
-        guess_x = int(input("Guess Row:"))
-        guess_y = int(input("Guess Column:"))
-        if board[guess_x][guess_y] == "X" or board[guess_x][guess_y] == "!":
-            print ("You already guessed there!")
-            turns -= 1
-        elif Assault(guess_x,guess_y) == True:
-            print ("You got a hit!")
-            board[guess_x][guess_y] = "!"
-            print_board(board)
-            print ("Ships sank:",BattleShip.sank)
-        else:
-            print ("Miss!")
-            board[guess_x][guess_y] = "X"
-            turns -= 1
-    else:
-        print ("Sorry, out of turns.")
-        break
-else:
-    print ("You won.")
+
+
+
+my_board = Board()
+my_game = Game()
+current_player = Player(input("What's your name, player?"))
+my_game.play()
+
+
+
+
+
+
+
